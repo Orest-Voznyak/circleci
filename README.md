1fb886eb5f79e57214c57ed40eb7c43fda62c93e
apiliuk <lohika.artur@98point6.com>
2019-04-16 15:40:03 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# To run tests
+* clone the project 
+```git clone```
+* open the project in PyCharm
+* [configure PyCharm to use pytest as default test runner](https://stackoverflow.com/a/6397315/1562282)
+* click on green triangle near test that you want to run
+
+# HTML Report
+![Alt text](doc/html.png?raw=true "HTML Report")
+# Allure Report
+![Alt text](doc/allure.png?raw=true "Allure Report")
+# Re-run failed tests
+![Alt text](doc/re-run.png?raw=true "Re-run Failed")
---
+++ src/pages/login_page.py
@@ -0,0 +1,34 @@
+import allure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LoginPage:
+    LOGIN_INPUT = (By.ID, "login-form-username")
+    PASSWORD_INPUT = (By.ID, "login-form-password")
+    LOGIN_BUTTON = (By.ID, "login")
+    driver = None
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    @allure.step
+    def login_to_jira(self):
+        self.driver.find_element(*self.LOGIN_INPUT).clear()
+        self.driver.find_element(*self.LOGIN_INPUT).send_keys("webinar5")
+        self.driver.find_element(*self.PASSWORD_INPUT).clear()
+        self.driver.find_element(*self.PASSWORD_INPUT).send_keys("webinar5")
+        self.driver.find_element(*self.LOGIN_BUTTON).submit()
+
+    @allure.step
+    def click_login_button(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(*self.LOGIN_INPUT)).click()
+
+    def at_page(self):
+        return "System Dashboard - Hillel IT School JIRA" in self.driver.title
+
+    @allure.step
+    def open(self):
+        self.driver.get("http://jira.hillel.it:8080/secure/Dashboard.jspa")
+        return self
