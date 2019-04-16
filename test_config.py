4c2f0fa0a4e31f5ecf7f60cbe3ae7feb5a08ddb6
apiliuk <lohika.artur@98point6.com>
2019-04-16 15:59:31 +0300

    fix

--- test_config.py
+++
@@ -1,8 +0,0 @@
-global config
-config = {
-  'myapp': {
-        'servers': {
-            'main_server': '.'.join(('10', '1', '1', '1',)),
-        }
-    }
-}
--- tests/ui/test_jira_login_ui_page_object.py
+++ tests/ui/test_jira_login_ui_page_object.py
@@ -7,7 +7,7 @@ from src.pages.login_page import LoginPage
 class TestLogin:
 
     def setup_method(self):
-        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+       # self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
         self.login_page = LoginPage(self.driver)
         self.header_page = HeaderPage(self.driver)
 
