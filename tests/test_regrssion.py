cb670facfdd7b62ec546cd401966b8815a77484b
apiliuk <lohika.artur@98point6.com>
2019-05-21 15:13:23 +0300

    fix

--- tests/test_regrssion.py
+++ tests/test_regrssion.py
@@ -3,14 +3,6 @@ import pytest
 
 class TestSample:
 
-    @pytest.mark.smoke
-    def test_smoke(self):
-        assert True
-
-    @pytest.mark.smoke
-    def test_smoke(self):
-        assert True
-
     def test_regression(self):
         assert True
 
---
+++ tests/test_smoke.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestSample:
+
+    @pytest.mark.smoke
+    def test_smoke(self):
+        assert True
+
+    @pytest.mark.smoke
+    def test_smoke(self):
+        assert True
